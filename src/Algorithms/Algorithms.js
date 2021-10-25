d4238147c77140f05cbc7472901a87e2c6c23125
moustapharamadan <moustapharamadan@hotmail.com>
2021-10-25 15:32:28 +0300

    Use random integer numbers

--- src/Algorithms/Algorithms.js
+++ src/Algorithms/Algorithms.js
@@ -1,14 +1,14 @@
 import { store } from "../Redux/store";
 import { toggleIsSortRunning, updateDataArray } from "../Redux/actions";
 
-function getRandomFloat(min, max) {
-  return Math.random() * (max - min + 1) + min;
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export function resetArray(size) {
   const array = [];
   for (let i = 0; i < size; i++) {
-    const rand = getRandomFloat(1, 100);
+    const rand = getRandomInteger(1, 100);
     array.push(rand);
   }
   return array;
